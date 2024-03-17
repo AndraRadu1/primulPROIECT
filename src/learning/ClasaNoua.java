115f1024e975abfef26a6baf1f28354336fbb288
Andra Radu <andra_v89@yahoo.com>
2024-03-17 12:35:09 +0200

    1st commit

---
+++ src/learning/ClasaNoua.java
@@ -0,0 +1,10 @@
+package learning;
+
+public class ClasaNoua {
+
+
+
+public static void main(String[] args) {;
+
+    System.out.println("Hello Skilbrain");
+    {
---
+++ src/learning/Primitive.java
@@ -0,0 +1,20 @@
+package learning;
+
+public class Primitive {
+    public static void main(String[] args) {
+        int numar;
+        numar = 5;
+         int numar2 =6;
+         float numarcuvirgula = 7.3F;
+         double numarCuPunct=7.3;
+         char unSingurCaracter= 'G';
+         long numarFoarteMare=1235647788899;
+         boolean trueSauFalse = true;
+         boolean trueSauFalse = false;
+         byte valoareMaxima = -22;
+
+
+
+
+    }
+}
